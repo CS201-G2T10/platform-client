01b05d2d72d4c3317332df71a3aa21ebd868cb96
Jonathan Chow <jonathanchowjh@gmail.com>
2020-11-09 11:30:22 +0800

    final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { useState } from 'react'
+import Loader from 'react-loader-spinner'
+import Modal from 'react-modal';
 import './App.css';
 // import qs from 'qs'
 
 /* eslint-disable no-console */
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 const Page = () => {
   const [word, setWord] = useState('');
-  const [num, setNum] = useState(30);
+  const [num, setNum] = useState(5);
   const [sentences, setSentences] = useState([]);
+  
+  // MODAL SPINNER
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const makeRequest = () => {
     const fetchParams = {
       headers: {
         'Accept': 'application/json',
@@ -24,22 +40,54 @@ const Page = () => {
     fetch("http://localhost:8080/api/model/predict", fetchParams)
       .then(res => res.json())
       .then((res) => {
+        closeModal()
         setSentences([
-          res,
+          {
+            joke: res.joke,
+            first_word: word,
+            max_length: num
+          },
           ...sentences
         ])
       })
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+  }
+
+  const handleSubmit = () => {
+    if (modalIsOpen) return;
+    openModal();
+    makeRequest();
   }
   return (
     <div className="page">
+      <Modal
+        isOpen={modalIsOpen}
+        // onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <br/>
+        <div>
+          <div className="bigger">Loading Joke</div>
+          <div>Might take some time if words needs to be compiled</div>
+        </div>
+        <br/><br/>
+        <Loader
+          type="Grid"
+          color="#41487d"
+          height={80}
+          width={80}
+          //  timeout={3000} //3 secs
+        /><br/>
+      </Modal>
       <div className="page-header">
-        <div>Generating Sentences</div>
+        <div>Generating Sentences (Jokes)</div>
       </div>
       <div className="page-input">
-        <form onSubmit={handleSubmit}>
+        <div className="page-input-div">
           <div>
-            <div>Enter a Word</div>
+            <div>Enter a first word of Joke :</div>
+            <div>Leave blank auto generate first word</div>
             <input
               type="text"
               onChange={(e) => { setWord(e.target.value) }}
@@ -48,18 +96,15 @@ const Page = () => {
             />
           </div>
           <div>
-            <div>Enter a Number</div>
+            <div>Enter max length of Joke :</div>
             <input
               type="number"
               onChange={(e) => { setNum(e.target.value) }}
               value={num}
             />
           </div>
-          <input
-            type="submit"
-            value="GENERATE Sentence!"
-          />
-        </form>
+          <button onClick={handleSubmit}>GENERATE a Joke!</button>
+        </div>
       </div>
       <div className="page-list">
         <div className="page-list-header">Sentences:</div>
@@ -67,17 +112,9 @@ const Page = () => {
           {sentences.map((sentence) => {
             return (
               <div className="page-list-sentence">
-                <div>
-                  Generation Parameters:&nbsp;
-                  &quot;
-                  {sentence.joke}
-                  &quot;
-                  &nbsp; Length:&nbsp;
-                  {sentence.max_length}
-                </div>
-                <div className="bigger">
-                  {sentence.first_word}
-                </div>
+                &quot;
+                {`${sentence.first_word} ${sentence.joke}`}
+                &quot;
               </div>
             )
           })}
